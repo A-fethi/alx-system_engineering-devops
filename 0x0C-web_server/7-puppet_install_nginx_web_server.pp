92335b640e33b4dc32866250aec0541680ab5101
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-26 09:37:05 -0700

    Adding forgoten service restart and fixing some syntax errors

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # Installs Nginx web server (w/ Puppet)
 
 exec { 'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo bash -c "echo Hello World!" > /var/www/html/index.nginx-debian.html ; sudo sed -i "s#server_name _;#server_name _;\n\trewrite ^\/redirect_me https:#/#/github.com#/A-fethi permanent;#" /etc/nginx/sites-enabled/default',
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo bash -c "echo Hello World!" > /var/www/html/index.nginx-debian.html ; sudo sed -i "s#server_name _;#server_name _;\n\trewrite ^\/redirect_me https://github.com/A-fethi permanent;#" /etc/nginx/sites-available/default ; sudo service nginx restart',
   provider => shell,
 }

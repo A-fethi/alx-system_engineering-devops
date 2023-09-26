56a573ebd3d9baf55369e1c7b458e0a8784ac5db
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-26 04:37:52 -0700

    first steps still have to be completed

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Installs Nginx web server (w/ Puppet)
+
+exec { 'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx \
+	       sudo bash -c "echo 'Hello World!' > /var/www/html/index.nginx-debian.html"',
+  provider => shell,
+}

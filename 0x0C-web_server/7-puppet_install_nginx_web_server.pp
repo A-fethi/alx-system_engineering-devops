08d54de6c48b651466d58c55f5172b3a2829732a
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-26 05:13:27 -0700

    enhacement

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bsah
 # Installs Nginx web server (w/ Puppet)
 
 exec { 'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx \
-	       sudo bash -c "echo 'Hello World!' > /var/www/html/index.nginx-debian.html"',
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo bash -c echo "Hello World!" > /var/www/html/index.nginx-debian.html',
   provider => shell,
 }

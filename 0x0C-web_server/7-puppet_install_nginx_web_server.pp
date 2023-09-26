900e2fe3abafa9a9cd74368fc40474f05548030b
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-26 08:57:47 -0700

    Please work ...

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
 # Installs Nginx web server (w/ Puppet)
 
 exec { 'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo bash -c "echo Hello World!" > /var/www/html/index.nginx-debian.html ; sudo sed -i "s#server_name _;#server_name _;\n\trewrite ^\/redirect_me https://github.com/A-fethi permanent;#" /etc/nginx/sites-enabled/default',
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo bash -c "echo Hello World!" > /var/www/html/index.nginx-debian.html ; sudo sed -i "s#server_name _;#server_name _;\n\trewrite ^\/redirect_me https:#/#/github.com#/A-fethi permanent;#" /etc/nginx/sites-enabled/default',
   provider => shell,
 }

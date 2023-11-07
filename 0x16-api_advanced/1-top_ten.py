7477c04620947f304683c89b0ef3fc37c9cab5cb
A-fethi <fethiabderrahmane1@gmail.com>
2023-11-07 05:43:49 -0800

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and
+prints the titles of the first 10 hot posts listed for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Returns the first 10 hot posts ."""
+    headers = {'User-Agent': 'A-fethi/1.0'}
+    r = requests.get(f'https://www.reddit.com/r/{subreddit}.json',
+                     headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        data = r.json()
+        for child in data['data']['children'][:10]:
+            title = child['data']['title']
+            print(title)
+    else:
+        print("None")

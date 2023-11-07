c9a277d16f583a3d0181af43d8b0aa93b6387f67
A-fethi <fethiabderrahmane1@gmail.com>
2023-11-07 05:47:07 -0800

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import requests
 def top_ten(subreddit):
     """Returns the first 10 hot posts ."""
     headers = {'User-Agent': 'A-fethi/1.0'}
-    r = requests.get(f'https://www.reddit.com/r/{subreddit}.json',
+    r = requests.get(f'https://www.reddit.com/r/{subreddit}/hot.json',
                      headers=headers, allow_redirects=False)
     if r.status_code == 200:
         data = r.json()

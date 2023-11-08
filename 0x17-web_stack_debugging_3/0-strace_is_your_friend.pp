a5f3fee489c88a041ab5678aafbc9c9089dd2649
A-fethi <fethiabderrahmane1@gmail.com>
2023-11-08 08:42:14 -0800

    Fixing puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fixing problem
 exec { 'Fix wordpress':
-  command => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
   provider => shell,
-} 
+}
